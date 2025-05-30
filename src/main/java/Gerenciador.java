7e7b6f640d9063c70f5192dc509bf5347d1a0bff
besinhorelli <bernardosinhorelli348@gmail.com>
2025-05-30 20:47:31 -0300

    adicionando interface grafica

--- src/main/java/Gerenciador.java
+++ src/main/java/Gerenciador.java
@@ -2,17 +2,41 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import models.Usuario;
+import org.hibernate.mapping.PrimaryKey;
 
-public class Gerenciador {
-    private static EntityManagerFactory emf;
+import javax.swing.*;
+import java.awt.*;
+
+public class Gerenciador extends JFrame {
+    private  CardLayout cardLayout;
+    private JPanel mainPanel;
+    private final String EMPTY_SCREEN = "EMPTY_SCREEN";
+
+    public Gerenciador() {
+        setTitle("gerenciador de projetos");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        this.cardLayout = new CardLayout();
+        this.mainPanel = new JPanel(cardLayout);
+        JPanel emptyPanel = new JPanel(new BorderLayout());
+        emptyPanel.add(new JLabel("bem-vindo! use o menu para navegar", SwingConstants.CENTER), BorderLayout.CENTER);
+        this.mainPanel.add(emptyPanel, EMPTY_SCREEN);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("menu");
+        JMenuItem listUserItem = new JMenuItem("Listar Usuarios");
+        JMenuItem ExitItem = new JMenuItem("Sair");
+        menu.add(listUserItem);
+        menu.add(ExitItem);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+        add(mainPanel);
+
+    }
 
     public static void main(String[] args) {
-        emf = Persistence.createEntityManagerFactory("PU");
-        EntityManager em = emf.createEntityManager();
-
-        Usuario usuario = new Usuario(1, "muttley", "muttley@gmail.com", "1245");
-        em.getTransaction().begin(); //abre para gravar o usuario
-        em.persist(usuario); //grava o usuario
-        em.getTransaction().commit(); //fecha após gravar o usuario
+        SwingUtilities.invokeLater(() -> {
+            new Gerenciador().setVisible(true);
+        });
     }
 }
