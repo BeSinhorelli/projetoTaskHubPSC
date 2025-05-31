32e2ab80078ecc9a2291dfbbd2a6f8b1cdff0d19
besinhorelli <bernardosinhorelli348@gmail.com>
2025-05-30 21:32:17 -0300

    feat: lista de usuarios

--- src/main/java/Gerenciador.java
+++ src/main/java/Gerenciador.java
@@ -3,11 +3,13 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import models.Usuario;
 import org.hibernate.mapping.PrimaryKey;
+import views.user.UsuarioList;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Gerenciador extends JFrame {
+    private final String LIST_USER = "LIST_USER";
     private  CardLayout cardLayout;
     private JPanel mainPanel;
     private final String EMPTY_SCREEN = "EMPTY_SCREEN";
@@ -21,19 +23,30 @@ public class Gerenciador extends JFrame {
         this.mainPanel = new JPanel(cardLayout);
         JPanel emptyPanel = new JPanel(new BorderLayout());
         emptyPanel.add(new JLabel("bem-vindo! use o menu para navegar", SwingConstants.CENTER), BorderLayout.CENTER);
+        UsuarioList usuarioList = new UsuarioList();
         this.mainPanel.add(emptyPanel, EMPTY_SCREEN);
+        this.mainPanel.add(usuarioList, LIST_USER);
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("menu");
         JMenuItem listUserItem = new JMenuItem("Listar Usuarios");
+        JMenuItem listTaskItem = new JMenuItem("Listar tarefas");
         JMenuItem ExitItem = new JMenuItem("Sair");
         menu.add(listUserItem);
+        menu.add(listTaskItem);
         menu.add(ExitItem);
         menuBar.add(menu);
         setJMenuBar(menuBar);
         add(mainPanel);
-
+        listUserItem.addActionListener(e -> {
+            this.cardLayout.show(mainPanel, LIST_USER);
+        });
+        listTaskItem.addActionListener(e -> {
+           JOptionPane.showMessageDialog(this, "Lista de projetos só se eu repetir a UC!");
+        });
+        ExitItem.addActionListener( e -> {
+            dispose();
+        });
     }
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new Gerenciador().setVisible(true);
